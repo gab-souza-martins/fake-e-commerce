bd1a11af139663fa5001a2721f6d25b40d8c8065
Grey-B <gabriellemener@gmail.com>
2025-10-08 11:56:27 -0300

    Adicionada função ngOnInit para item-list.ts para pegar os itens da API

--- src/app/components/item-list/item-list.ts
+++ src/app/components/item-list/item-list.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FakeStore } from '../../services/fake-store';
+import { Item } from '../../types/item.type';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-item-list',
   imports: [],
   templateUrl: './item-list.html',
-  styleUrl: './item-list.css'
+  styleUrl: './item-list.css',
 })
-export class ItemList {
+export class ItemList implements OnInit {
+  fakeStore = inject(FakeStore);
+  itemList = signal<Array<Item>>([]);
 
+  ngOnInit(): void {
+    this.fakeStore
+      .getItems()
+      .pipe(
+        catchError((e) => {
+          console.log(e);
+          throw e;
+        })
+      )
+      .subscribe((i) => this.itemList.set(i));
+  }
 }
