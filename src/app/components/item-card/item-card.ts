b6de83f0eeae50803273b86f3c169299d60a3004
Grey-B <gabriellemener@gmail.com>
2025-10-11 14:26:47 -0300

    Adicionados ícones do FontAwesome, e ícone de estrela em item-card

--- src/app/components/item-card/item-card.ts
+++ src/app/components/item-card/item-card.ts
@@ -1,12 +1,15 @@
 import { Component, input } from '@angular/core';
 import { Item } from '../../types/item.type';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-item-card',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './item-card.html',
   styleUrl: './item-card.css',
 })
 export class ItemCard {
   item = input.required<Item>();
+  faStar = faStar;
 }
