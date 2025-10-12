eecc8decddcf48dde84ea24661cc5e621f8c51ce
Grey-B <gabriellemener@gmail.com>
2025-10-12 11:49:56 -0300

    product-page agora recebem o produto completo baseado em seu id

--- src/app/product-page/product-page.ts
+++ src/app/product-page/product-page.ts
@@ -1,4 +1,7 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, signal } from '@angular/core';
+import { Item } from '../types/item.type';
+import { FakeStore } from '../services/fake-store';
+import { catchError } from 'rxjs';
 
 @Component({
   selector: 'app-product-page',
@@ -8,4 +11,35 @@ import { Component, input } from '@angular/core';
 })
 export class ProductPage {
   productId = input.required<string>();
+
+  fakeStore = inject(FakeStore);
+  product = signal<Item>({
+    id: 0,
+    title: '',
+    price: 0,
+    description: '',
+    category: '',
+    image: '',
+    rating: {
+      rate: 0,
+      count: 0,
+    },
+  });
+
+  ngOnInit(): void {
+    this.fakeStore
+      .getItems()
+      .pipe(
+        catchError((e) => {
+          console.log(e);
+          throw e;
+        })
+      )
+      .subscribe((i) => {
+        const product = i.find((p) => p.id.toString() === this.productId());
+        if (product) {
+          this.product.set(product);
+        }
+      });
+  }
 }
