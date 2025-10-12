c5f0c809586ed87c17b9ad88222216b9532e79cd
Grey-B <gabriellemener@gmail.com>
2025-10-12 11:37:03 -0300

    product-page agora corretamente recebe seu id como um input

--- src/app/product-page/product-page.ts
+++ src/app/product-page/product-page.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-product-page',
   imports: [],
   templateUrl: './product-page.html',
-  styleUrl: './product-page.css'
+  styleUrl: './product-page.css',
 })
 export class ProductPage {
-
+  productId = input.required<string>();
 }
