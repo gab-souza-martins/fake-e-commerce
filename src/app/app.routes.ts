05a3c33bdad6c5b281ce5465fb9d387f13cb6dd7
Grey-B <gabriellemener@gmail.com>
2025-10-12 11:27:25 -0300

    Adicionada rota dinâmica para novo componente product-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { ProductPage } from './product-page/product-page';
 
 export const routes: Routes = [
   {
     path: '',
     component: Home,
+  },
+  {
+    path: 'product/:productId',
+    component: ProductPage,
   },
 ];
