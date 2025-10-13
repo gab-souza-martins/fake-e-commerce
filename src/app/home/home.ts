a60b2cf0e9ca4a41ba1658d1ec1b7d630b9bc418
Grey-B <gabriellemener@gmail.com>
2025-10-13 10:14:28 -0300

    header passado de app.html para header.html, e lista de categorias passada de home.html para header.html

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
 import { ItemList } from '../components/item-list/item-list';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [ItemList, RouterLink],
+  imports: [ItemList],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
