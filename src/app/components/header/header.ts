d6ec6c2e16fa27dffcb30a5cfa6ac462fe50b623
Grey-B <gabriellemener@gmail.com>
2025-10-13 12:11:43 -0300

    Criados breakpoints para ativar ou desativar a visibilidade de cada navegação

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header {
+  desktopNavigationActive = false;
+
+  constructor(private breakpointService: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.breakpointService
+      .observe([Breakpoints.Medium, Breakpoints.Large, Breakpoints.XLarge])
+      .subscribe((result) => {
+        this.desktopNavigationActive = false;
+        if (result.matches) {
+          this.desktopNavigationActive = true;
+        }
+      });
+  }
+}
