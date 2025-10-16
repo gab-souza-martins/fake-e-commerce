e1cc76a5c98edadc00895e4d320813206b8285d6
Grey-B <gabriellemener@gmail.com>
2025-10-16 15:11:11 -0300

    checkout-page agora recebe os itens do carrinho. Por enquanto, exibe cart-item, porém um componente checkout-item será criado

--- src/app/checkout-page/checkout-page.ts
+++ src/app/checkout-page/checkout-page.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CartStorage } from '../services/cart-storage';
+
+// TODO: Apagar e substituir pelo componente de item de checkout
+import { CartItemComponent } from '../components/cart-item/cart-item';
 
 @Component({
   selector: 'app-checkout-page',
-  imports: [],
+  imports: [CartItemComponent],
   templateUrl: './checkout-page.html',
-  styleUrl: './checkout-page.css'
+  styleUrl: './checkout-page.css',
 })
 export class CheckoutPage {
-
+  constructor(private cartStorage: CartStorage) {}
+  get cartItems() {
+    return this.cartStorage.getCart();
+  }
 }
