969b53a4cf54f2e8cc76f824a7ab26b295e1df84
Grey-B <gabriellemener@gmail.com>
2025-10-16 11:23:54 -0300

    Função setItem em cart-storage.ts renomeada para setCart, e adicionadas funções getCart e clearCart

--- src/app/services/cart-storage.ts
+++ src/app/services/cart-storage.ts
@@ -5,11 +5,29 @@ import { CartItem } from '../types/cart-item.type';
   providedIn: 'root',
 })
 export class CartStorage {
-  setItem(value: CartItem): void {
+  setCart(value: CartItem): void {
     try {
       localStorage.setItem('localCart', JSON.stringify(value));
     } catch (e) {
       console.error('Error saving to local storage', e);
+    }
+  }
+
+  getCart(): CartItem[] | null {
+    try {
+      const data: string | null = localStorage.getItem('localCart');
+      return data ? JSON.parse(data) : null;
+    } catch (e) {
+      console.error('Error reading from local storage', e);
+      return null;
+    }
+  }
+
+  clearCart(): void {
+    try {
+      localStorage.removeItem('localCart');
+    } catch (e) {
+      console.error('Error removing from local storage', e);
     }
   }
 }
