f97a57eb62099e01cbe08aae69c3cf43a4b9c21d
Grey-B <gabriellemener@gmail.com>
2025-10-16 12:41:58 -0300

    Adicionado botão de remover item do carrinho para cart-item.html, div.container de cart-item agora é um grid ao invés de um flexbox, e largura de div.cart-container em header foi alterada novamente

--- src/app/components/cart-item/cart-item.ts
+++ src/app/components/cart-item/cart-item.ts
@@ -1,12 +1,15 @@
 import { Component, input } from '@angular/core';
 import { CartItem } from '../../types/cart-item.type';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faX } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-cart-item',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './cart-item.html',
   styleUrl: './cart-item.css',
 })
 export class CartItemComponent {
   item = input.required<CartItem>();
+  faX = faX;
 }
