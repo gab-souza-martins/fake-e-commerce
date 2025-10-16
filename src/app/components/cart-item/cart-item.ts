b5ed6c97fbf57afcf504b79012f2558644f4a16e
Grey-B <gabriellemener@gmail.com>
2025-10-16 12:49:23 -0300

    Adicionada função removeItem para cart-item.ts para remover um item do carrinho

--- src/app/components/cart-item/cart-item.ts
+++ src/app/components/cart-item/cart-item.ts
@@ -2,6 +2,7 @@ import { Component, input } from '@angular/core';
 import { CartItem } from '../../types/cart-item.type';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faX } from '@fortawesome/free-solid-svg-icons';
+import { CartStorage } from '../../services/cart-storage';
 
 @Component({
   selector: 'app-cart-item',
@@ -12,4 +13,18 @@ import { faX } from '@fortawesome/free-solid-svg-icons';
 export class CartItemComponent {
   item = input.required<CartItem>();
   faX = faX;
+
+  constructor(private cartStorage: CartStorage) {}
+
+  removeItem(): void {
+    const currentCart: CartItem[] | null = this.cartStorage.getCart();
+    let newCart: CartItem[] = [];
+
+    if (currentCart) {
+      newCart = currentCart;
+    }
+    newCart = newCart.filter((i) => i.id !== this.item().id);
+
+    this.cartStorage.setCart(newCart);
+  }
 }
