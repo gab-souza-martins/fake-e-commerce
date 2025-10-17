f9854be0aed64af1e0e46a992ee91a236d6c6a89
Grey-B <gabriellemener@gmail.com>
2025-10-17 11:16:08 -0300

    checkout-page agora mostra o subtotal do preço de todos os itens

--- src/app/checkout-page/checkout-page.ts
+++ src/app/checkout-page/checkout-page.ts
@@ -10,7 +10,20 @@ import { CheckoutItem } from '../components/checkout-item/checkout-item';
 })
 export class CheckoutPage {
   constructor(private cartStorage: CartStorage) {}
+
   get cartItems() {
     return this.cartStorage.getCart();
+  }
+
+  get totalPrice() {
+    let total: number = 0;
+
+    if (this.cartItems) {
+      for (let i = 0; i < this.cartItems.length; i++) {
+        total += this.cartItems[i].itemInfo.price;
+      }
+    }
+
+    return total;
   }
 }
