dbb6cdeb7d68bcbff94e586bb7c8da3721f3a381
Grey-B <gabriellemener@gmail.com>
2025-10-19 10:26:38 -0300

    Adicionadas funções de salvar e buscar token do usuário e de cadastrar, e propri edade de token para user.type.ts

--- src/app/services/login-service.ts
+++ src/app/services/login-service.ts
@@ -1,8 +1,32 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginService {
-  
+  saveUserToken(token: string): void {
+    try {
+      localStorage.setItem('userToken', token);
+    } catch (e) {
+      console.error('Error saving user token to local storage', e);
+    }
+  }
+
+  getUserToken(): string | null {
+    try {
+      return localStorage.getItem('userToken');
+    } catch (e) {
+      console.error('Error retrieving user token from local storage', e);
+      return null;
+    }
+  }
+
+  signIn(username: string, email: string, password: string): boolean {
+    if (username && email && password) {
+      const mockToken = crypto.randomUUID();
+      this.saveUserToken(mockToken);
+      return true;
+    }
+    return false;
+  }
 }
