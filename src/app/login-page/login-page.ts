ccd6be85a4e5a5e6cbb0df74ace5d7a38e449bee
Grey-B <gabriellemener@gmail.com>
2025-10-19 12:01:56 -0300

    Adicionado ícone para botão de login em login-page.html e alterados tamanhos do form.login-form e botão de login

--- src/app/login-page/login-page.ts
+++ src/app/login-page/login-page.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './login-page.html',
-  styleUrl: './login-page.css'
+  styleUrl: './login-page.css',
 })
 export class LoginPage {
-
+  faLogin = faRightToBracket;
 }
