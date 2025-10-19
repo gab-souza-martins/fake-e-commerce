122780f59ab3b4fa53beaffbca72d6b420b03b92
Grey-B <gabriellemener@gmail.com>
2025-10-19 12:18:26 -0300

    Adicionado controle de form reativo para login-page

--- src/app/login-page/login-page.ts
+++ src/app/login-page/login-page.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators } from '@angular/forms';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faRightToBracket } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login-page',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
+  fb = new FormBuilder();
+  loginForm = this.fb.group({
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    passwordConfirm: ['', [Validators.required, Validators.minLength(8)]],
+    postalCode: ['', [Validators.required, Validators.minLength(3)]],
+  });
+  submitted = false;
+
   faLogin = faRightToBracket;
 }
--- src/app/types/user.type.ts
+++ src/app/types/user.type.ts
@@ -2,6 +2,6 @@ export interface User {
   token: string;
   username: string;
   email: string;
-  postalCode: string;
   password: string;
+  postalCode: string;
 }
